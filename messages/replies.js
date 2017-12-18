70811f418f7320047fee2466ccb209fc6940b7f7
Geoff Affleck <geoff.affleck@feralcatlabs.com>
2017-12-18 15:33:08 -0800

    start of complext messaging

---
+++ messages/replies.js
@@ -0,0 +1,44 @@
+var messages = {
+	
+	NOT_FOUND: {
+    "text": "I can't find that command",
+    "attachments": [
+        {
+            "text": "What would you like to do?",
+            "fallback": "Help not supported",
+            "callback_id": "nocommand_nextstep",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "selection",
+                    "text": "Try Another Command",
+                    "type": "button",
+                    "value": "tryagain"
+                },
+                {
+                    "name": "selection",
+                    "text": "See All Options",
+                    "type": "button",
+                    "value": "seeall"
+                },
+                {
+                    "name": "game",
+                    "text": "Ask for help",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "help",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "We'll message your manager and ask for help",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+}
+}
+
+module.exports = messages;
