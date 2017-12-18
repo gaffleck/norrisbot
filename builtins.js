7616da4c7c8ff586392e44ccfee3d5a21caced64
Geoff Affleck <geoff.affleck@feralcatlabs.com>
2017-12-18 14:33:36 -0800

    removing sqlite db until it's needed

--- builtins.js
+++ builtins.js
@@ -9,7 +9,8 @@ module.exports = {
     'What is left to do ',
     'What\'s remainig', 
     'Give me all tasks', 
-    'How much is left do do?'
+    'How much is left do do?',
+    'all tasks'
   ] 
   
 };
