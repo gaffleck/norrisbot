235837a96d889f6914bc65e78ffdb4598a6de899
Geoff Affleck <geoff.affleck@feralcatlabs.com>
2017-12-18 00:18:48 -0800

    adding natural library and first intents

--- bin/bot.js
+++ bin/bot.js
@@ -3,16 +3,29 @@
 'use strict';
 
 var NorrisBot = require('../lib/norrisbot');
+var Brain = require('../lib/brain');
 
 
 var token = process.env.SLACK_TOKEN;
 var dbPath = process.env.BOT_DB_PATH;
 var name = 'onboardingroboto';
+var _brain = new Brain();
+
+var builtinPhrases = require('../builtins');
+
+console.log('learning...');
+Object.keys(builtinPhrases).forEach(function(key) {
+    _brain.teach(key, builtinPhrases[key]);
+  });
+_brain.think();
+console.log('finished learning');
 
 var norrisbot = new NorrisBot({
     token: token,
     dbPath: dbPath,
-    name: name
+    name: name, 
+    brain: _brain
 });
 
 norrisbot.run();
+
---
+++ builtins.js
@@ -0,0 +1,15 @@
+module.exports = {
+  GetNextTask: [
+    'What\'s next?',
+    'What do I do next?', 
+    'What is my next task?', 
+    'What should I do now?'
+  ], 
+  GetAllTask: [
+    'What is left to do ',
+    'What\'s remainig', 
+    'Give me all tasks', 
+    'How much is left do do?'
+  ] 
+  
+};
